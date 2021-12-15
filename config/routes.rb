32f9434e4dda7b9111bfb0c79c8fd64c227c989a
Dilshan <dilshan.mudali@gmail.com>
2021-12-14 22:24:14 -0500

    route fix

--- config/routes.rb
+++ config/routes.rb
@@ -17,5 +17,7 @@ Rails.application.routes.draw do
 
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
+
+  get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
