1090210dbc1b2ecfcca451039edf70243f17d2eb
Dilshan <dilshan.mudali@gmail.com>
2022-06-09 06:59:38 -0700

    updated version

--- client/src/Components/Main/Customers/Customers.jsx
+++ client/src/Components/Main/Customers/Customers.jsx
@@ -1,13 +1,12 @@
 import { useState, useContext } from 'react';
 import globalContext from '../../../Context/globalContext';
 import RenderCustomers from './RenderCustomers';
-import useApiCall from '../../../hooks/use-fetch';
+
 const Customers = () => {
   const context = useContext(globalContext);
   const customers = context.customers;
   const setCustomers = context.setCustomers;
   const userId = context.user.id;
-  const { fetchData, isloading, error } = useApiCall();
   const [addCustomer, setAddCustomer] = useState({
     user_id: userId,
     customer_name: '',
@@ -23,24 +22,14 @@ const Customers = () => {
     });
   };
 
-  const submitCustomerData = async () => {
-    const data = await fetchData('/customers', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify(addCustomer),
-    });
-
-    if (error) {
-      console.log(error);
-    }
-
-    return data;
-  };
-
   const handleSubmit = (e) => {
     e.preventDefault();
     //setting new customer
-    submitCustomerData()
+    fetch('/customers', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(addCustomer),
+    })
       .then((r) => r.json())
       .then((newCustomer) => {
         setCustomers([...customers, newCustomer]);
@@ -61,10 +50,6 @@ const Customers = () => {
       setCustomers(custLeft);
     });
   };
-
-  if (isloading) {
-    return '...loading';
-  }
 
   return (
     <div className='customer-container'>
